f3a3c0074458c8a8cd74f2b39bed8a9fde71fa7a
Vachell <vastiansen@gmail.com>
2022-11-16 20:54:48 +0700

    view

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
-// connection
+app.get("/", async (req, res) => {
+  res.render("index")
+});
+
+// connection for local server
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
